4eb9b5e610a8ee9f871ba2a89d77084a653ab5e5
FeezyHendrix <hafeezraheem@gmail.com>
2018-11-24 21:29:17 +0100

    initial commit

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -132,4 +132,4 @@ STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
     ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
