f42328f61cc9bb0254992f7f7d788810e5918ff4
FeezyHendrix <hafeezraheem@gmail.com>
2018-11-24 23:10:12 +0100

    initial commit

--- urlshortener/wsgi.py
+++ urlshortener/wsgi.py
@@ -14,5 +14,4 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'urlshortener.settings')
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
